2acc2b289b9086acae8b1a88627d71c3c31a058b
AvavSam <avav2608005@gmail.com>
2025-06-07 13:06:13 +0800

    feat(app): implement URL scheme enforcement and gate definitions
    Added URL scheme enforcement for production environment and defined
    authorization gates for admin and land viewing permissions.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,27 +4,31 @@ namespace App\Providers;
 
 use Gate;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        //
-    }
+  /**
+   * Register any application services.
+   */
+  public function register(): void
+  {
+    //
+  }
 
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot(): void
-    {
-        Gate::define('admin', function ($user) {
-            return $user->isAdmin();
-        });
-        Gate::define('view-land', function ($user, $land) {
-            return $user->isAdmin();
-        });
+  /**
+   * Bootstrap any application services.
+   */
+  public function boot(): void
+  {
+    if (env('APP_ENV') === 'production') {
+      URL::forceScheme('https');
     }
+    Gate::define('admin', function ($user) {
+      return $user->isAdmin();
+    });
+    Gate::define('view-land', function ($user, $land) {
+      return $user->isAdmin();
+    });
+  }
 }
